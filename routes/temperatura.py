3b94e1a62f7f4e53a2d2b31358b815ef8c2ea65e
gab-villani <gabrielavillany70@gmail.com>
2023-11-26 21:40:55 -0400

    viscosidade e massa especifica

--- routes/temperatura.py
+++ routes/temperatura.py
@@ -1,2 +1,21 @@
+from scipy.interpolate import interp1d
 
-            
+# Dados fornecidos
+temperaturas =        [0.01,       10,         20,         30,        40,        50,        60,        70,        80,       90,       100,      110,      120,      130,      140,      150]
+densidade_liquida =   [1000,       999.7,      998.2,      995.6,     992.2,     988.0,     983.2,     977.7,     971.8,    965.3,    958.3,    950.9,    943.1,    934.8,    926.1,    917.0]
+viscosidade_liquida = [0.00000922, 0.00000946, 0.00000973, 0.0000100, 0.0000103, 0.0000106, 0.0000109, 0.0000113, 0.0000116,0.0000119,0.0000123,0.0000126,0.0000130,0.0000133,0.0000137,0.0000140]
+
+# Funções de interpolação
+densidade_interp = interp1d(temperaturas, densidade_liquida, kind='cubic')
+viscosidade_interp = interp1d(temperaturas, viscosidade_liquida, kind='cubic')
+
+def propriedades_agua(temperatura):
+    # Interpolação para encontrar densidade e viscosidade
+    densidade = densidade_interp(temperatura)
+    viscosidade = viscosidade_interp(temperatura)
+    
+    # Calcular massa específica (inverso da densidade)
+    massa_especifica = 1 / densidade
+    
+    return massa_especifica, viscosidade
+
--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from routes.route import tempe_c, vazao, rotacao, forca, pressao
+from routes.temperatura import propriedades_agua
 from src.config import Config
 
 class MyApp:
@@ -13,25 +14,34 @@ class MyApp:
         self.label.grid(row=0, column=0, columnspan=2, pady=10)
 
         # Entry widgets for displaying values
-        ttk.Label(root, text="Pressure:").grid(row=1, column=0)
+        ttk.Label(root, text="Pressao:").grid(row=1, column=0)
         self.pressure_entry = ttk.Entry(root, width=20)
         self.pressure_entry.grid(row=1, column=1, pady=5)
 
-        ttk.Label(root, text="Force:").grid(row=2, column=0)
+        ttk.Label(root, text="Forca:").grid(row=2, column=0)
         self.force_entry = ttk.Entry(root, width=20)
         self.force_entry.grid(row=2, column=1, pady=5)
 
-        ttk.Label(root, text="Temperature:").grid(row=3, column=0)
+        ttk.Label(root, text="Temperatura:").grid(row=3, column=0)
         self.tempe_entry = ttk.Entry(root, width=20)
         self.tempe_entry.grid(row=3, column=1, pady=5)
 
-        ttk.Label(root, text="Rotation:").grid(row=4, column=0)
+        ttk.Label(root, text="Rotacao:").grid(row=4, column=0)
         self.rotation_entry = ttk.Entry(root, width=20)
         self.rotation_entry.grid(row=4, column=1, pady=5)
 
-        ttk.Label(root, text="Flow:").grid(row=5, column=0)
+        ttk.Label(root, text="Vazao:").grid(row=5, column=0)
         self.flow_entry = ttk.Entry(root, width=20)
         self.flow_entry.grid(row=5, column=1, pady=5)
+
+        # Entry widgets for displaying mass and viscosity
+        ttk.Label(root, text="Massa Especifica:").grid(row=6, column=0)
+        self.mass_entry = ttk.Entry(root, width=30)
+        self.mass_entry.grid(row=6, column=1, pady=5)
+
+        ttk.Label(root, text="Viscosidade:").grid(row=7, column=0)
+        self.viscosity_entry = ttk.Entry(root, width=30)
+        self.viscosity_entry.grid(row=7, column=1, pady=5)
 
         # Atualiza os valores a cada 1000 milissegundos (1 segundo)
         self.root.after(1000, self.update_values)
@@ -44,7 +54,7 @@ class MyApp:
             pressure = pressao[-1] if pressao else 0
             flow = vazao[-1] if vazao else 0
             force = forca[-1] if forca else 0
-
+            
             # Display the values in the entry widgets
             self.tempe_entry.delete(0, tk.END)
             self.tempe_entry.insert(0, temperature)
@@ -60,6 +70,15 @@ class MyApp:
 
             self.force_entry.delete(0, tk.END)
             self.force_entry.insert(0, force)
+
+            # Calculate and display mass specific and viscosity
+            massa_especifica, viscosidade = propriedades_agua(temperature)
+
+            self.mass_entry.delete(0, tk.END)
+            self.mass_entry.insert(0, massa_especifica)
+
+            self.viscosity_entry.delete(0, tk.END)
+            self.viscosity_entry.insert(0, viscosidade)
 
         except Exception as e:
             # Handle exceptions, e.g., invalid input
